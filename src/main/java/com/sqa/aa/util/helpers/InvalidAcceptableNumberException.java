9e3f16f2f7e39a23bd4eaa6ef80837101c5bf44f
Anna Antonyan <=>
2016-04-02 10:46:44 -0700

    Adding InvalidAcceptableNumberException class to helper methods

---
+++ src/main/java/com/sqa/aa/util/helpers/InvalidAcceptableNumberException.java
@@ -0,0 +1,27 @@
+/**
+ *   File Name: InvalidAcceptableNumberException.java<br>
+ *
+ *   LastName, FirstName<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Apr 2, 2016
+ *   
+ */
+
+package com.sqa.aa.util.helpers;
+
+/**
+ * InvalidAcceptableNumberException //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author      LastName, FirstName
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+public class InvalidAcceptableNumberException extends Exception {
+
+}
